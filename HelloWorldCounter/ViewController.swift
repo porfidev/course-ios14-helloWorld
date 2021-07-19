6f68dc53c05184e4a1d819b2a6a5ae3aaf3b3fdf
porfi.dev <elporfirio@gmail.com>
2021-07-19 14:27:08 -0500

    ADD: reset button

--- HelloWorldCounter/ViewController.swift
+++ HelloWorldCounter/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter = 0
 
+    @IBOutlet weak var labelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // labelText.text = "Hola Mundo"
+        labelText.text = String(counter)
     }
 
-
+    @IBAction func counterAction(_ sender: Any) {
+        counter += 1
+        labelText.text = String(counter)
+    }
+    
+    @IBAction func resetAction(_ sender: Any) {
+        counter = 0
+        labelText.text = String(counter)
+    }
 }
 
